55047a992b462002b61a05b3454a5d1d1c4e61e9
Lokesh Sathish <f20220732@hyderabad.bits-pilani.ac.in>
2026-04-15 16:45:35 +0530

    Add API permission test - check which ApplicationManager methods are allowed

--- service/main.js
+++ service/main.js
@@ -9,6 +9,21 @@ var measurement = new MeasurementService(service);
 // Initialize on first load
 measurement.init();
 
+// Debug: test which Application Manager methods are permitted
+(function testAPIs() {
+  var testAPIs = [
+    'luna://com.webos.applicationManager/running',
+    'luna://com.webos.applicationManager/listLaunchPoints',
+    'luna://com.webos.applicationManager/listApps',
+    'luna://com.webos.applicationManager/getForegroundAppInfo'
+  ];
+  testAPIs.forEach(function(uri) {
+    service.call(uri, {}, function(res) {
+      console.log('[API TEST] ' + uri + ' => ' + JSON.stringify(res).substring(0, 200));
+    });
+  });
+})();
+
 // Handle relaunch events
 service.register('__webOSRelaunch', function (message) {
   measurement.handleRelaunch();
